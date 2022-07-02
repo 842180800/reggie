08b68571375da75bae0f51f11237ecf618db4cf4
hhb <hhb@qq.com>
2022-07-02 12:49:29 +0800

    fourth commit

--- src/main/java/com/itheima/reggie/CodeGenerator.java
+++ src/main/java/com/itheima/reggie/CodeGenerator.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 
 public class CodeGenerator {
 
-//test 3版本  hot-fix2.add
+//test 3版本  fourth commit
     public static void main(String[] args) {
 
         FastAutoGenerator.create("jdbc:mysql://175.24.181.119:3306/reggie?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false&allowPublicKeyRetrieval=true",
